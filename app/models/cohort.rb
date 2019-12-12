afd60e9ae524b5faf826ae1ae1acb315129c8b7b
jstrombe19 <jaredstromberg@gmail.com>
2019-12-12 16:13:40 -0700

    Added validations to all attributes of cohort and student models, with the exception of the previous_partners on students.

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,5 @@
 class Cohort < ApplicationRecord
   has_many :students, :dependent => :destroy
+  validates :name, presence: true
+  validates :graduated, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,9 @@
 class Student < ApplicationRecord
   belongs_to :cohort
+  validates :name, presence: true
+  validates :cohort_id, presence: true
+  validates :blue, presence: true
+  validates :gold, presence: true
+  validates :green, presence: true
+  validates :orange, presence: true
 end
