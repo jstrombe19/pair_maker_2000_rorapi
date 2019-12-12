eda6826899adfce0d42f4a0848919cb0deec64f6
jstrombe19 <jaredstromberg@gmail.com>
2019-12-12 16:07:58 -0700

    Clarified the relationship between students and cohorts so that if a cohort is deleted, all students within that cohort are also deleted.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students
-  resources :cohorts
+  resources :students, except: [:new, :edit, :update]
+  resources :cohorts, except: [:new, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
