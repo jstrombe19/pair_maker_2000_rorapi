f4a70fe600fbbf174e4f7e4d419702d463b520e3
jstrombe19 <jaredstromberg@gmail.com>
2019-12-12 15:37:34 -0700

    Built out index, show, create, and destroy in cohorts_controller, as well as the private methods for strong params and getting all cohorts or a single cohort by id.

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,2 +1,52 @@
 class CohortsController < ApplicationController
+  before_action: :get_all_cohorts, only: [:index, :destroy]
+  before_action: :find_cohort_by_id, only: [:show, :destroy]
+
+  def index
+    render json: @cohorts
+  end
+
+  def show
+    render json: @cohort 
+  end
+
+  def create
+    @cohort = Cohort.new(cohort_params)
+    if @cohort.save
+      render json: @cohort
+    else
+      render json: error.message
+    end
+  end
+
+  def destroy
+    @cohort.destroy 
+    render json: @cohorts 
+  end
+
+  private
+
+  def cohort_params
+    params.require(:cohort).permit(:name, :graduated, :id)
+  end
+
+  def get_all_cohorts
+    @cohorts = Cohort.includes(:students).all 
+  end
+
+  def find_cohort_by_id
+    @cohort = Cohort.includes(:students).find(params[:id])
+  end
+
 end
+
+# @companies = Company.joins(:persons).where(:persons => { active: true } ).all
+# @companies.each do |company| 
+#   company.name 
+# end
+
+
+# @companies = Company.includes(:persons).where(:persons => { active: true } ).all
+# @companies.each do |company|
+#      company.person.name
+# end
